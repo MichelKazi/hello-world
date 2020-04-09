c321e94fd0956f6904086af16144ddf685809a27
michelkazi <michel.m.kazi@gmail.com>
2020-04-08 21:54:49 -0400

    added patch request, also uses formdata instead of rawjson

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
@@ -116,6 +117,15 @@ func main() {
 		}
 	})
 
+	r.PATCH("/person/update/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		newName := c.PostForm("Person")
+		patchPersonName(name, newName, db)
+		param := getPerson(newName, db)
+
+		c.JSON(http.StatusOK, param)
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
@@ -147,4 +157,11 @@ func deletePerson(name string, db *sql.DB) bool {
 		return false
 	}
 	return true
+}
+
+func patchPersonName(name string, newName string, db *sql.DB) {
+	_, err := db.Exec(`UPDATE helloworld.person SET Person = $1 WHERE Person = $2`, newName, name)
+	if err != nil {
+		panic(err)
+	}
 }
